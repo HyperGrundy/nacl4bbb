e68f0218b4383c84cf8f3cf29739fa96ccc0619d
Grundy <grundy@hyperboria.name>
2013-06-03 10:31:38 -0700

    Files compiled with: -O2 -march=armv7-a -mtune=cortex-a8 -mfpu=vfpv3 -ftree-vectorize -fomit-frame-pointer

---
+++ include/crypto_types.h
@@ -0,0 +1,9 @@
+#ifndef crypto_types_h
+#define crypto_types_h
+typedef unsigned short crypto_uint16;
+typedef unsigned long long crypto_uint64;
+typedef unsigned char crypto_uint8;
+typedef signed char crypto_int8;
+typedef short crypto_int16;
+typedef unsigned int crypto_uint32;
+#endif
